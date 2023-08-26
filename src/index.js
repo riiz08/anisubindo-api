dd8b0a5be16dc85b40fbadb4893351f80fc86c63
riiz08 <mhmdrizki0812@gmail.com>
2023-08-26 19:28:19 +0700

    change dir folder

--- src/index.js
+++ src/index.js
@@ -8,51 +8,118 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
+var __generator = (this && this.__generator) || function (thisArg, body) {
+    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
+    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
+    function verb(n) { return function (v) { return step([n, v]); }; }
+    function step(op) {
+        if (f) throw new TypeError("Generator is already executing.");
+        while (g && (g = 0, op[0] && (_ = 0)), _) try {
+            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
+            if (y = 0, t) op = [op[0] & 2, t.value];
+            switch (op[0]) {
+                case 0: case 1: t = op; break;
+                case 4: _.label++; return { value: op[1], done: false };
+                case 5: _.label++; y = op[1]; op = [0]; continue;
+                case 7: op = _.ops.pop(); _.trys.pop(); continue;
+                default:
+                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
+                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
+                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
+                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
+                    if (t[2]) _.ops.pop();
+                    _.trys.pop(); continue;
+            }
+            op = body.call(thisArg, _);
+        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
+        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
+    }
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = __importDefault(require("express"));
-const cors_1 = __importDefault(require("cors"));
-const service_1 = require("./service");
-const app = (0, express_1.default)();
-const port = 5000;
+var express_1 = require("express");
+var cors_1 = require("cors");
+var service_1 = require("./service");
+var app = (0, express_1.default)();
+var port = 5000;
 app.use((0, cors_1.default)());
-app.get("/", (req, res) => {
+app.get("/", function (req, res) {
     res.send("hello im riiz");
 });
-app.get("/popular-today", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
-    try {
-        const data = yield (0, service_1.getMostPopularToday)();
-        res.json(data);
-    }
-    catch (e) {
-        res.send(e.message);
-    }
-}));
-app.get("/latest-release", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
-    const page = Number(req.query.page) || 1;
-    try {
-        const data = yield (0, service_1.getLatestRelease)(page);
-        res.json(data);
-    }
-    catch (e) {
-        res.send(e.message);
-    }
-}));
-app.get("/anime/", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
-    const { page, genre, season, studio, status, type, sub, order } = req.query;
-    try {
-        const data = yield (0, service_1.filteringAnime)(page, genre, season, studio, status, type, sub, order);
-        res.json(data);
-    }
-    catch (err) {
-        res.send(err.message);
-    }
-}));
-app.get("/detail/:slug", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
-    const { slug } = req.params;
-    const data = yield (0, service_1.getDetailAnime)(slug);
-    res.json(data);
-}));
-app.listen(port, () => console.log(`app running in port : ${port}`));
+app.get("/popular-today", function (req, res) { return __awaiter(void 0, void 0, void 0, function () {
+    var data, e_1;
+    return __generator(this, function (_a) {
+        switch (_a.label) {
+            case 0:
+                _a.trys.push([0, 2, , 3]);
+                return [4 /*yield*/, (0, service_1.getMostPopularToday)()];
+            case 1:
+                data = _a.sent();
+                res.json(data);
+                return [3 /*break*/, 3];
+            case 2:
+                e_1 = _a.sent();
+                res.send(e_1.message);
+                return [3 /*break*/, 3];
+            case 3: return [2 /*return*/];
+        }
+    });
+}); });
+app.get("/latest-release", function (req, res) { return __awaiter(void 0, void 0, void 0, function () {
+    var page, data, e_2;
+    return __generator(this, function (_a) {
+        switch (_a.label) {
+            case 0:
+                page = Number(req.query.page) || 1;
+                _a.label = 1;
+            case 1:
+                _a.trys.push([1, 3, , 4]);
+                return [4 /*yield*/, (0, service_1.getLatestRelease)(page)];
+            case 2:
+                data = _a.sent();
+                res.json(data);
+                return [3 /*break*/, 4];
+            case 3:
+                e_2 = _a.sent();
+                res.send(e_2.message);
+                return [3 /*break*/, 4];
+            case 4: return [2 /*return*/];
+        }
+    });
+}); });
+app.get("/anime/", function (req, res) { return __awaiter(void 0, void 0, void 0, function () {
+    var _a, page, genre, season, studio, status, type, sub, order, data, err_1;
+    return __generator(this, function (_b) {
+        switch (_b.label) {
+            case 0:
+                _a = req.query, page = _a.page, genre = _a.genre, season = _a.season, studio = _a.studio, status = _a.status, type = _a.type, sub = _a.sub, order = _a.order;
+                _b.label = 1;
+            case 1:
+                _b.trys.push([1, 3, , 4]);
+                return [4 /*yield*/, (0, service_1.filteringAnime)(page, genre, season, studio, status, type, sub, order)];
+            case 2:
+                data = _b.sent();
+                res.json(data);
+                return [3 /*break*/, 4];
+            case 3:
+                err_1 = _b.sent();
+                res.send(err_1.message);
+                return [3 /*break*/, 4];
+            case 4: return [2 /*return*/];
+        }
+    });
+}); });
+app.get("/detail/:slug", function (req, res) { return __awaiter(void 0, void 0, void 0, function () {
+    var slug, data;
+    return __generator(this, function (_a) {
+        switch (_a.label) {
+            case 0:
+                slug = req.params.slug;
+                return [4 /*yield*/, (0, service_1.getDetailAnime)(slug)];
+            case 1:
+                data = _a.sent();
+                res.json(data);
+                return [2 /*return*/];
+        }
+    });
+}); });
+app.listen(port, function () { return console.log("app running in port : ".concat(port)); });
