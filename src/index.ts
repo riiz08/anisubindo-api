8d8cd87a89db319fd388b7f30df6fdd4e9a81425
riiz08 <mhmdrizki0812@gmail.com>
2023-08-29 04:39:24 +0700

    fix cors

--- src/index.ts
+++ src/index.ts
@@ -11,6 +11,9 @@ import {
 
 const app = express();
 const port = 5000;
+axios.defaults.validateStatus = () => true;
+axios.defaults.headers.common["User-Agent"] =
+  "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36 Edg/108.0.1462.54";
 
 app.use(cors());
 
